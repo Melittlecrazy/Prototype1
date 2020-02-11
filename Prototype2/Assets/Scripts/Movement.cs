571ea1d26500ae902cbb4df543f4fa61ba9f40e3
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-02-10 22:12:51 -0500

    added an animal game

---
+++ Prototype2/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    [SerializeField]
+    private GameObject RightBorder;
+    private float RightBorderXValue;
+    [SerializeField]
+    private GameObject LeftBorder;
+    private float LeftBorderXValue;
+
+    void Start()
+    {
+        Transform RightTransform = RightBorder.GetComponent<Transform>();
+        RightBorderXValue = RightTransform.position.x;
+
+        Transform LeftTransform = LeftBorder.GetComponent<Transform>();
+        LeftBorderXValue = LeftTransform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+
+            if (this.transform.position.x >= RightBorderXValue)
+            {
+                transform.rotation = Quaternion.FromToRotation(Vector3.left, Vector3.forward);
+            }
+            if (this.transform.position.x <= LeftBorderXValue)
+            {
+
+                transform.rotation = Quaternion.FromToRotation(Vector3.right, Vector3.forward);
+            }
+        }
+    }
+}
