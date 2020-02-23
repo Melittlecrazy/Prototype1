c968e9248f29ddbfcd35d637f9f713f11c88e55b
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-02-23 15:16:27 -0600

    made the player jump and die

---
+++ Prototype3/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    public GameObject obstaclePrefab;
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
